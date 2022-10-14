daf3e627383723af7f637390eecc3ad98e9f86ab
Asmaa <asmaa.alamir456@gmail.com>
2022-10-14 12:51:55 +0200

    transferStartup

--- MicroRabbit.Transfer.Api/Startup.cs
+++ MicroRabbit.Transfer.Api/Startup.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroRabbit.infra.Ioc;
+using Microsoft.EntityFrameworkCore;
+using MicroRabbit.Transfer.Data.Context;
 
 namespace MicroRabbit.Transfer.Api
 {
@@ -25,14 +29,22 @@ namespace MicroRabbit.Transfer.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<TransferDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TransferDbConnection"));
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MicroRabbit.Transfer.Api", Version = "v1" });
             });
+            services.AddMediatR(typeof(Startup));
+            RegisterServices(services);
         }
-
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
